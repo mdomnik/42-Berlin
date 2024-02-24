a8cb5272f7a6186ee27375252a2c7d8dadd661c8
Maciej Domnik <mdomnik@c4a11c3.42berlin.de>
2024-02-24 20:11:56 +0100

    norminetted minitalk

--- Projects/minitalk/client.c
+++ Projects/minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: mdomnik <mdomnik@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:46:54 by mdomnik           #+#    #+#             */
-/*   Updated: 2024/02/24 20:05:36 by mdomnik          ###   ########.fr       */
+/*   Updated: 2024/02/24 20:10:37 by mdomnik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,19 @@ void	send_message(int pid, char *message)
 	int	letter;
 	int	bit_count;
 	int	bit;
-	
+
 	i = 0;
-	while(message[i] != '\0')
+	while (message[i] != '\0')
 	{
 		letter = message[i];
 		bit_count = 0;
 		while (bit_count <= 7)
-		{	
+		{
 			bit = (letter >> bit_count) & 1; 
-			if(bit == 0)
-			{
+			if (bit == 0)
 				kill(pid, SIGUSR1);
-			}
 			else
-			{
 				kill(pid, SIGUSR2);
-			}
 			usleep(100);
 			bit_count++;
 		}
@@ -46,14 +42,14 @@ void	send_message(int pid, char *message)
 	}
 }
 
-int main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	int	pid;
 	int	i;
 
 	i = 0;
 	if (argc != 3)
-		ft_printf("wrong amount of parameters | input PID and the message you want to send\n");
+		ft_printf("wrong amount of parameters! (enter PID and message)\n");
 	else
 	{
 		pid = ft_atoi(argv[1]);
--- Projects/minitalk/server.c
+++ Projects/minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: mdomnik <mdomnik@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:47:00 by mdomnik           #+#    #+#             */
-/*   Updated: 2024/02/24 20:08:31 by mdomnik          ###   ########.fr       */
+/*   Updated: 2024/02/24 20:11:40 by mdomnik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,29 @@
 #include <unistd.h>
 #include <signal.h>
 
-
-void signal_handler(int	signal)
+void	signal_handler(int signal)
 {
-	static unsigned char		letter;
-	static int		iter = 0;
+	static unsigned char	letter;
+	static int				iter = 0;
 
 	if (signal == SIGUSR1)
 		letter |= (0 << iter);
 	else
 		letter |= (1 << iter);
 	iter++;
-	if(iter == 8)
+	if (iter == 8)
 	{
 		ft_putchar_fd(letter, 1);
 		iter = 0;
 		letter = 0;
 	}
 }
-int main (void)
+
+int	main(void)
 {
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
-	ft_printf("Server PID [%d] | establish connection using ./client <PID> <message>\n", getpid());
+	ft_printf("Server PID [%d] | ./client <PID> <message>\n", getpid());
 	while (1)
 		sleep(1);
 	return (0);
